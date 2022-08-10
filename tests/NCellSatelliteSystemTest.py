23d070bbc7ee77110132234975de49061b2bfb88
JustinLaw-D <justin11002@gmail.com>
2022-08-10 13:43:33 -0700

    finished testing

--- tests/NCellSatelliteSystemTest.py
+++ tests/NCellSatelliteSystemTest.py
@@ -8,11 +8,14 @@ dh = 50 # height of band (km)
 alts = np.arange(600, 900+dh/2, dh)
 alt_edges = np.arange(575, 925+1, dh)
 V = 4*np.pi*dh*(R+alts)**2 # volume of band
-S_i = [[0,0,0]]*len(alts)
-D_i = [[0,0,0]]*len(alts)
-S_di = [[0,0,0]]*len(alts)
+S_i = []
+D_i = []
+S_di = []
 N_i = np.zeros(len(alts))
 for i in range(len(alts)):
+    S_i.append([0,0,0])
+    D_i.append([0,0,0])
+    S_di.append([0,0,0])
     N_i[i] = 2.5e-8*V[i]
 lam = [1000, 1000, 1000]
 T = 60
